b7b336a358e7738bddc8ef6274e3d7ab0657e795
Eduardo Ryô Tamaki <eduardo.rtamaki@gmail.com>
2020-09-04 15:17:29 -0300

    Add files via upload

---
+++ R Scripts/functions/check_na.R
@@ -0,0 +1,32 @@
+# ---
+# Tratamento do Banco de Dados e Criação da Variável: Atitudes Populistas
+# Atitudes Populistas e Voto em Bolsonaro em 2018 (2020)
+# ---
+# Eduardo Ryô Tamaki
+# Mestrando em Ciência Política da UFMG
+# e-mail: eduardo.rtamaki@gmail.com
+# ---
+# 14/08/2020
+# ---
+
+
+## PREAMBULO -------------------------------------------------------------------
+
+
+# Função para trazer quais colunas possuem algum valor NA e a quantidade deles:
+
+
+## Carregando o pacote "purrr"
+library(purrr)
+
+
+check_NA = function(x) {
+  for (i in 1:length(x)){
+    if(map(x[i], ~sum(is.na(.))) > 0) {
+      print(map(x[i], ~sum(is.na(.))))
+      
+    }
+    
+  }
+  
+}
