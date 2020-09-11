48d1554cb387271c0416869ba405be0c6ad8a4f1
Eduardo Ryô Tamaki <eduardo.rtamaki@gmail.com>
2020-09-11 20:10:07 -0300

    Add files via upload

---
+++ Scripts/02 - Regressoes.R
@@ -0,0 +1,294 @@
+# ---
+# 02 - Regressões
+# Regressões para diferntes modelos
+# Atitudes Populistas e Voto em Bolsonaro em 2018 (2020)
+# ---
+# Eduardo Ryô Tamaki
+# Mestrando em Ciência Política da UFMG
+# e-mail: eduardo.rtamaki@gmail.com
+# ---
+# 11/09/2020
+# ---
+
+
+## PREAMBULO -------------------------------------------------------------------
+
+library(tidyverse)
+library(here)
+library(stargazer)
+library(car)
+
+
+source(here::here("00 - Tratando e Criando as Variaveis.R"), encoding = "UTF-8")
+
+
+###
+# Regressões:
+
+
+#######################
+# Atitudes Populistas #
+#######################
+
+
+###
+# Modelo 1: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Atitudes Populistas, Sexo, Idade, Educação
+
+m1 <- glm(voto_b ~ pop_2c + sexo + id + ed, 
+            data = e19,
+            na.action = na.omit,
+            family = binomial(link = "logit"),
+            maxit = 100)
+# Exponencial:
+exp1 <- exp(m1$coefficients)
+# P-valor:
+p1 <- list(summary(m1)$coefficients[,4])
+
+
+###
+# Modelo 2: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Atitudes Populistas * Ideologia, Sexo, Idade, Educação  
+m2 <- glm(voto_b ~ pop_2c * ideo2.2 + sexo + id + ed, 
+          data = e19,
+          na.action = na.omit,
+          family = binomial(link = "logit"),
+          maxit = 100)
+# Exponencial:
+exp2 <- exp(m2$coefficients)
+# P-valor:
+p2 <- list(summary(m2)$coefficients[,4])
+
+
+###
+# Modelo 3: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Atitudes Populistas * Ideologia, Sexo, Idade, Educação, Corrupção
+m3 <- glm(voto_b ~ pop_2c * ideo2.2 + sexo + id + ed + corrup1, 
+          data = e19,
+          na.action = na.omit,
+          family = binomial(link = "logit"),
+          maxit = 100)
+# Exponencial:
+exp3 <- exp(m3$coefficients)
+# P-valor:
+p3 <- list(summary(m3)$coefficients[,4])
+
+
+###
+# Modelo 4: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Atitudes Populistas * Ideologia, Sexo, Idade, Educação, Corrupção, PC2
+m4 <- glm(voto_b ~ pop_2c * ideo2.2 + sexo + id + ed + corrup1 + PC2, 
+          data = e19,
+          na.action = na.omit,
+          family = binomial(link = "logit"),
+          maxit = 100)
+# Exponencial:
+exp4 <- exp(m4$coefficients)
+# P-valor:
+p4 <- list(summary(m4)$coefficients[,4])
+
+
+###
+# Modelo 5: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Atitudes Populistas * Ideologia, Sexo, Idade, Educação, Corrupção, PC2, 
+    # Antipetismo
+m5 <- glm(voto_b ~ pop_2c * ideo2.2 + sexo + id + ed + corrup1 + PC2 + antipt, 
+          data = e19,
+          na.action = na.omit,
+          family = binomial(link = "logit"),
+          maxit = 100)
+# Exponencial:
+exp5 <- exp(m5$coefficients)
+# P-valor:
+p5 <- list(summary(m5)$coefficients[,4])
+
+
+###
+# Modelo 6: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Atitudes Populistas * Ideologia, Sexo, Idade, Educação, Corrupção, PC2, 
+    # Antipetismo, Renda
+m6 <- glm(voto_b ~ pop_2c * ideo2.2 + sexo + id + ed + corrup1 + PC2 + antipt + 
+            fx_renda,
+          data = e19,
+          na.action = na.omit,
+          family = binomial(link = "logit"),
+          maxit = 100)
+# Exponencial:
+exp6 <- exp(m6$coefficients)
+# P-valor:
+p6 <- list(summary(m6)$coefficients[,4])
+
+
+###
+# Modelo 7: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Atitudes Populistas * Ideologia, Sexo, Idade, Educação, Corrupção, PC2, 
+    # Antipetismo, Renda, Religião
+m7 <- glm(voto_b ~ pop_2c * ideo2.2 + sexo + id + ed + corrup1 + PC2 + antipt + 
+            fx_renda + relig,
+          data = e19,
+          na.action = na.omit,
+          family = binomial(link = "logit"),
+          maxit = 100)
+# Exponencial:
+exp7 <- exp(m7$coefficients)
+# P-valor:
+p7 <- list(summary(m7)$coefficients[,4])
+
+
+
+
+
+#############################################
+# Tipologia Atitudes Populistas x Ideologia #
+#############################################
+
+###
+# Modelo 1: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Tipologia Atitudes Populistas, Sexo, Idade, Educação
+
+mt1 <- glm(voto_b ~ pop_ed_sart + sexo + id + ed, 
+          data = e19,
+          na.action = na.omit,
+          family = binomial(link = "logit"),
+          maxit = 100)
+# Exponencial:
+exp_t1 <- exp(mt1$coefficients)
+# P-valor:
+p_t1 <- list(summary(mt1)$coefficients[,4])
+
+
+
+###
+# Modelo 2: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Tipologia Atitudes Populistas, Sexo, Idade, Educação, Corrupção
+
+mt2 <- glm(voto_b ~ pop_ed_sart + sexo + id + ed + corrup1, 
+           data = e19,
+           na.action = na.omit,
+           family = binomial(link = "logit"),
+           maxit = 100)
+# Exponencial:
+exp_t2 <- exp(mt2$coefficients)
+# P-valor:
+p_t2 <- list(summary(mt2)$coefficients[,4])
+
+
+
+###
+# Modelo 3: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Tipologia Atitudes Populistas, Sexo, Idade, Educação, Corrupção,
+    # PC2
+
+mt3 <- glm(voto_b ~ pop_ed_sart + sexo + id + ed + corrup1 + PC2, 
+           data = e19,
+           na.action = na.omit,
+           family = binomial(link = "logit"),
+           maxit = 100)
+# Exponencial:
+exp_t3 <- exp(mt3$coefficients)
+# P-valor:
+p_t3 <- list(summary(mt3)$coefficients[,4])
+
+
+
+###
+# Modelo 4: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Tipologia Atitudes Populistas, Sexo, Idade, Educação, Corrupção,
+    # PC2, Antipetismo
+
+mt4 <- glm(voto_b ~ pop_ed_sart + sexo + id + ed + corrup1 + PC2 + antipt, 
+           data = e19,
+           na.action = na.omit,
+           family = binomial(link = "logit"),
+           maxit = 100)
+# Exponencial:
+exp_t4 <- exp(mt4$coefficients)
+# P-valor:
+p_t4 <- list(summary(mt4)$coefficients[,4])
+
+
+
+###
+# Modelo 5: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Tipologia Atitudes Populistas, Sexo, Idade, Educação, Corrupção,
+    # PC2, Antipetismo
+
+mt5 <- glm(voto_b ~ pop_ed_sart + sexo + id + ed + corrup1 + PC2 + antipt + relig, 
+           data = e19,
+           na.action = na.omit,
+           family = binomial(link = "logit"),
+           maxit = 100)
+# Exponencial:
+exp_t5 <- exp(mt5$coefficients)
+# P-valor:
+p_t5 <- list(summary(mt5)$coefficients[,4])
+
+
+
+###
+# Modelo 6: 
+  # Var. Dep.: Voto em Bolsonaro
+  # Var. Ind.: Tipologia Atitudes Populistas, Sexo, Idade, Educação, Corrupção,
+    # PC2, Antipetismo
+
+mt6 <- glm(voto_b ~ pop_ed_sart + sexo + id + ed + corrup1 + PC2 + antipt + 
+             relig + fx_renda, 
+           data = e19,
+           na.action = na.omit,
+           family = binomial(link = "logit"),
+           maxit = 100)
+# Exponencial:
+exp_t6 <- exp(mt6$coefficients)
+# P-valor:
+p_t6 <- list(summary(mt6)$coefficients[,4])
+
+
+
+#####################
+###
+# VIFS das Regressões:
+
+vif(mt1)
+  # 1.18
+vif(mt2)
+  # 1.19
+vif(mt3)
+  # 1.17
+vif(mt4)
+  # 1.21
+vif(mt5)
+  # 1.21
+vif(mt6)
+  # 1.39
+
+
+vif(m1)
+  # 1.18
+vif(m2)
+  # 6.13
+vif(m3)
+  # 6.16
+vif(m4)
+  # 6.14
+vif(m5)
+  # 5.86
+vif(m6)
+  # 6.44 
+vif(m7)
+  # 6.49
+
+
+
+
---
+++ Scripts/03 - Tabelas Stargazer.R
@@ -0,0 +1,412 @@
+library(tidyverse)
+library(here)
+library(stargazer)
+
+
+source(here::here("00 - Tratando e Criando as Variaveis.R"), encoding = "UTF-8")
+source(here::here("02 - Regressoes.R"), encoding = "UTF-8")
+
+
+#################################
+# Tipologia Atitudes Populistas #
+################################# 
+
+###
+# Tabela 1: Modelo 1
+stargazer(title = "Modelo 1: Voto x Atitudes Populistas", 
+          m1, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista",
+                             "Sexo: Ser Homem",
+                             "Idade",
+                             "Educação"
+          ),
+          p.auto = F,
+          p = p1,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "01 - Voto x AP, Sexo, Idade, Ed.html")
+)
+
+
+###
+# Tabela 2: Modelo 2
+stargazer(title = "Modelo 2: Voto x Atitudes Populistas", 
+          m2, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista",
+                             "Ser de Direita",
+                             "Ser de Esquerda",
+                             "Sexo: Ser Homem",
+                             "Idade",
+                             "Educação",
+                             "Ser de Direita e Populista",
+                             "Ser de Esquerda e Populista"
+          ),
+          p.auto = F,
+          p = p2,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "02 - Voto x AP e Ideo, Sexo, Idade, Ed.html")
+)
+
+
+###
+# Tabela 3: Modelo 3
+stargazer(title = "Modelo 3: Voto x Atitudes Populistas", 
+          m3, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista",
+                             "Ser de Direita",
+                             "Ser de Esquerda",
+                             "Sexo: Ser Homem",
+                             "Idade",
+                             "Educação",
+                             "Percepção da Corrupção",
+                             "Ser de Direita e Populista",
+                             "Ser de Esquerda e Populista"
+          ),
+          p.auto = F,
+          p = p3,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "03 - Voto x AP e Ideo, Sexo, Idade, Ed, Corrup.html")
+)
+
+
+###
+# Tabela 4: Modelo 4
+stargazer(title = "Modelo 4: Voto x Atitudes Populistas", 
+          m4, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista",
+                             "Ser de Direita",
+                             "Ser de Esquerda",
+                             "Sexo: Ser Homem",
+                             "Idade",
+                             "Educação",
+                             "Percepção da Corrupção",
+                             "PC2",
+                             "Ser de Direita e Populista",
+                             "Ser de Esquerda e Populista"
+          ),
+          p.auto = F,
+          p = p4,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "04 - Voto x AP e Ideo, Sexo, Idade, Ed, Corrup, PC2.html")
+)
+
+
+###
+# Tabela 5: Modelo 5
+stargazer(title = "Modelo 5: Voto x Atitudes Populistas", 
+          m5, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista (ref.: Não Populista)",
+                             "Ser de Direita (ref.: Centro)",
+                             "Ser de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação",
+                             "Percepção da Corrupção",
+                             "PC2",
+                             "Ser Antipetista (ref.: Não)",
+                             "Ser de Direita e Populista (ref.: Centro e Populista)",
+                             "Ser de Esquerda e Populista"
+          ),
+          p.auto = F,
+          p = p5,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "05 - Voto x AP e Ideo, Sexo, Idade, Ed, Corrup, PC2, Antipt.html")
+)
+
+
+###
+# Tabela 6: Modelo 6
+stargazer(title = "Modelo 6: Voto x Atitudes Populistas", 
+          m6, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista (ref.: Não Populista)",
+                             "Ser de Direita (ref.: Centro)",
+                             "Ser de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação",
+                             "Percepção da Corrupção",
+                             "PC2",
+                             "Ser Antipetista (ref.: Não)",
+                             "Faixa de Renda",
+                             "Ser de Direita e Populista (ref.: Centro e Populista)",
+                             "Ser de Esquerda e Populista"
+          ),
+          p.auto = F,
+          p = p6,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "06 - Voto x AP e Ideo, Sexo, Idade, Ed, Corrup, PC2, Antipt, Renda.html")
+)
+
+
+###
+# Tabela 7: Modelo 7
+stargazer(title = "Modelo 7: Voto x Atitudes Populistas", 
+          m7, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista (ref.: Não Populista)",
+                             "Ser de Direita (ref.: Centro)",
+                             "Ser de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação",
+                             "Percepção da Corrupção",
+                             "PC2",
+                             "Ser Antipetista (ref.: Não)",
+                             "Faixa de Renda",
+                             "Religião: Evangélico (ref.: Outras)",
+                             "Ser de Direita e Populista (ref.: Centro e Populista)",
+                             "Ser de Esquerda e Populista"
+          ),
+          p.auto = F,
+          p = p7,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "07 - Voto x AP e Ideo, Sexo, Idade, Ed, Corrup, PC2, Antipt, Renda, Religiao.html")
+)
+
+
+###
+# Tabela 8: Setpwise Modelos 1-7
+
+
+md <- c("Base", "+ Interação com Ideologia",
+        "+ Corrupção", "+ PC2", "+ Antipetismo",
+        "+ Renda", "+ Religião")
+stargazer(title = "Modelo 7: Voto x Atitudes Populistas", 
+          m1, m2, m3, m4, m5, m6, m7, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = ("Voto em Bolsonaro x Outros"),
+          column.separate = c(1, 1, 1, 1, 1, 1, 1), 
+          column.labels = c(md),
+          #dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista (ref.: Não Populista)",
+                             "Ser de Direita (ref.: Centro)",
+                             "Ser de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação",
+                             "Percepção da Corrupção",
+                             "PC2",
+                             "Ser Antipetista (ref.: Não)",
+                             "Faixa de Renda",
+                             "Religião: Evangélico (ref.: Outras)",
+                             "Ser de Direita e Populista (ref.: Centro e Populista)",
+                             "Ser de Esquerda e Populista"
+          ),
+          p.auto = F,
+          p = c(p1, p2, p3, p4, p5, p6, p7),
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "08 - Voto x AP e Ideo, Sexo, Idade, Ed, Corrup, PC2, Antipt, Renda, Religiao.html")
+)
+
+
+
+
+#############################################
+# Tipologia Atitudes Populistas x Ideologia #
+#############################################
+
+
+# Tabela 10: Modelo 1
+stargazer(title = "Modelo 1: Voto x Tipologia Atitudes Populistas", 
+          mt1, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista de Centro(ref.: Não Populista)",
+                             "Populista de Direita",
+                             "Populista de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação"
+                             ),
+          p.auto = F,
+          p = p_t1,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "10 - Voto x TAP e Ideo, Sexo, Idade, Ed.html")
+)
+
+
+# Tabela 11: Modelo 2
+stargazer(title = "Modelo 2: Voto x Tipologia Atitudes Populistas", 
+          mt2, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista de Centro(ref.: Não Populista)",
+                             "Populista de Direita",
+                             "Populista de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação",
+                             "Corrupção"
+          ),
+          p.auto = F,
+          p = p_t2,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "11 - Voto x TAP e Ideo, Sexo, Idade, Ed, Corrup.html")
+)
+
+
+
+# Tabela 12: Modelo 3
+stargazer(title = "Modelo 3: Voto x Tipologia Atitudes Populistas", 
+          mt3, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista de Centro(ref.: Não Populista)",
+                             "Populista de Direita",
+                             "Populista de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação",
+                             "Corrupção",
+                             "PC2"
+          ),
+          p.auto = F,
+          p = p_t3,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "12 - Voto x TAP e Ideo, Sexo, Idade, Ed, Corrup, PC2.html")
+)
+
+
+# Tabela 13: Modelo 4
+stargazer(title = "Modelo 4: Voto x Tipologia Atitudes Populistas", 
+          mt4, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista de Centro(ref.: Não Populista)",
+                             "Populista de Direita",
+                             "Populista de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação",
+                             "Corrupção",
+                             "PC2",
+                             "Antipetismo"
+          ),
+          p.auto = F,
+          p = p_t4,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "13 - Voto x TAP e Ideo, Sexo, Idade, Ed, Corrup, PC2, Antipetismo.html")
+)
+
+
+# Tabela 14: Modelo 5
+stargazer(title = "Modelo 5: Voto x Tipologia Atitudes Populistas", 
+          mt5, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista de Centro(ref.: Não Populista)",
+                             "Populista de Direita",
+                             "Populista de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação",
+                             "Corrupção",
+                             "PC2",
+                             "Antipetismo",
+                             "Religião: Evangélico"
+          ),
+          p.auto = F,
+          p = p_t5,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "14 - Voto x TAP e Ideo, Sexo, Idade, Ed, Corrup, PC2, Antipetismo, Relig.html")
+)
+
+
+# Tabela 15: Modelo 6
+stargazer(title = "Modelo 6: Voto x Tipologia Atitudes Populistas", 
+          mt6, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = "Voto em Bolsonaro",
+          perl = TRUE,
+          covariate.labels=c("Populista de Centro(ref.: Não Populista)",
+                             "Populista de Direita",
+                             "Populista de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação",
+                             "Corrupção",
+                             "PC2",
+                             "Antipetismo",
+                             "Religião: Evangélico",
+                             "Renda"
+          ),
+          p.auto = F,
+          p = p_t6,
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "15 - Voto x TAP e Ideo, Sexo, Idade, Ed, Corrup, PC2, Antipetismo, Relig, Renda.html")
+)
+
+
+###
+# Tabela 16: Setpwise Modelos 1-6
+
+
+md <- c("Base", "+ Corrupção", "+ PC2", "+ Antipetismo",
+        "+ Religião",
+        "+ Renda")
+
+stargazer(title = "Modelo 7: Voto x Atitudes Populistas", 
+          mt1, mt2, mt3, mt4, mt5, mt6, 
+          header = F, type="latex",
+          dep.var.caption = c("Variável Dependente"),
+          dep.var.labels = ("Voto em Bolsonaro x Outros"),
+          column.separate = c(1, 1, 1, 1, 1, 1),
+          column.labels = c(md),
+          perl = TRUE,
+          covariate.labels=c("Populista de Centro(ref.: Não Populista)",
+                             "Populista de Direita",
+                             "Populista de Esquerda",
+                             "Sexo: Ser Homem (ref.: ser Mulher)",
+                             "Idade",
+                             "Educação",
+                             "Percepção da Corrupção",
+                             "PC2",
+                             "Ser Antipetista (ref.: Não)",
+                             "Religião: Evangélico (ref.: Outras)",
+                             "Faixa de Renda"
+          ),
+          p.auto = F,
+          p = c(p_t1, p_t2, p_t3, p_t4, p_t5, p_t6),
+          report = ('vc*sp'), out = here::here("anexos", 
+                                               "16 - Voto x TAP , Sexo, Idade, Ed, Corrup, PC2, Antipt, Renda, Religiao.html")
+)
+
